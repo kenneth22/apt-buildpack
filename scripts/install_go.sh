7083f9142021b2f663827c36e22c0ac66a21eaa6
Kenneth Caranyagan Dandan <Kenneth_C_Dandan@manulife.com>
2019-01-19 11:04:19 +0800

    test

--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -10,7 +10,8 @@ if [ ! -f $GoInstallDir/go/bin/go ]; then
   GO_MD5="0571886e9b9ba07773b542a11e9859a4"
   URL=https://buildpacks.cloudfoundry.org/dependencies/go/go${GO_VERSION}.linux-amd64-${GO_MD5:0:8}.tar.gz
 
-  echo "-----> Download go ${GO_VERSION} testestse"
+  echo "-----> Download go ${GO_VERSION}"
+  echo $URL
   curl -s -L --retry 15 --retry-delay 2 $URL -o /tmp/go.tar.gz
   echo "download step done"
   DOWNLOAD_MD5=$(md5sum /tmp/go.tar.gz | cut -d ' ' -f 1)
@@ -19,9 +20,11 @@ if [ ! -f $GoInstallDir/go/bin/go ]; then
     echo "       **ERROR** MD5 mismatch: got $DOWNLOAD_MD5 expected $GO_MD5"
     exit 1
   fi
-
+  ls =l $GoInstallDir
+  echo "EXTR2 $GoInstallDir"
   tar xzf /tmp/go.tar.gz -C $GoInstallDir
   rm /tmp/go.tar.gz
+  echo "EXTR1"
 fi
 if [ ! -f $GoInstallDir/go/bin/go ]; then
   echo "       **ERROR** Could not download go"
